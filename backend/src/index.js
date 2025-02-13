ae2e5ff3ce36367ca9a86e33df377f96b52ad2be
harshdevadiya <harshdevadiya.ce20@sltiet.edu.in>
2025-02-14 00:22:17 +0530

    -- middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,13 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
-
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://silentscribe.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+})
 
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
